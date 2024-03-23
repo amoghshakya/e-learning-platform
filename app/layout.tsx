26909697419560c96c3bc40e4ba54f319c4aad06
Amogh Shakya <amoghshakya@outlook.com>
2024-03-24 00:53:20 +0545

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,20 +5,22 @@ import { body } from "./fonts";
 import clsx from "clsx";
 import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "E-learning platform",
   description: "se project 4204",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="en">
-      <SessionProvider>
+      <SessionProvider session={session}>
         <body className={clsx(body.className, "antialiased")}>
           <Toaster />
           {children}
