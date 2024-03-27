d3c0ccc6d678e39b086f6c08248ecbc262eb91f1
Amogh Shakya <amoghshakya@outlook.com>
2024-03-27 09:11:25 +0545

    Fixing middleware for edge runtime

--- middleware.ts
+++ middleware.ts
@@ -6,33 +6,31 @@ import {
   authRoutes,
   publicRoutes,
 } from "@/routes";
-import { auth } from "./auth";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-export default async function middleware(req: NextRequest) {
-  await NextAuth(authConfig);
-  const session = await auth();
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const session = req.auth;
   const isLoggedIn = !!session?.user.id;
 
   const isApiAuthRoute = req.nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(req.nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(req.nextUrl.pathname);
-  if (isApiAuthRoute) return null;
+  if (isApiAuthRoute) return;
   if (isAuthRoute) {
     if (isLoggedIn) {
       return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, req.url));
     }
-    return null;
+    return;
   }
   if (!isLoggedIn && !isPublicRoute) {
     return NextResponse.redirect(new URL("/join/login", req.url));
   }
-  return null;
-}
+  return;
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
-// export const { auth: middleware } = NextAuth(authConfig);
