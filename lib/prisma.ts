9f9e2780dc899898778fc66bb6e385ac30d5e948
Amogh Shakya <amoghshakya@outlook.com>
2024-03-27 16:58:46 +0545

    Fixing middleware for edge runtime

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,9 @@
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 declare global {
   var prisma: PrismaClient;
 }
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
 const prisma = global.prisma || new PrismaClient({ log: ["query"] });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
